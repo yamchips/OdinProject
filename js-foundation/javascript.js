82583875a7077d4f5f0b3ce10892c80217bb818a
yamchips <wflunch@gmail.com>
2025-02-14 22:33:44 +0000

    Start learning javascript

---
+++ js-foundation/javascript.js
@@ -0,0 +1,13 @@
+let name1 = "John";
+let surname = "Doe";
+console.log(name1);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+pi = 10;
+console.log(pi)

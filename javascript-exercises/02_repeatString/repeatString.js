45df5d0d3818876c6ee468aacc5bb2a83c0f607f
yamchips <wflunch@gmail.com>
2025-02-20 21:41:12 +0000

    Finish js exercise 1 and 2

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (string === '') return '';
+    if (num < 0) return "ERROR";
+    let res = '';
+    for (let i = 0; i < num; i++) {
+        res += string;
+    }
+    return res;
 };
 
 // Do not edit below this line

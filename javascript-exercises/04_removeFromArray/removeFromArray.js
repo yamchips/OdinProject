2a03ba0abf567f444c2e06233f7621b152c5a559
yamchips <wflunch@gmail.com>
2025-02-21 11:41:55 +0000

    Finish js exercise 04 to 07

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    for (let arg of args) {    
+        while (arr.indexOf(arg) !== -1) {
+            let index = arr.indexOf(arg);
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let res = (temp - 32) * 5 / 9;
+  if (Number.isInteger(res)) {
+    return res;
+  } else {
+    return +res.toFixed(1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let res = temp * 9 / 5 + 32;
+  if (Number.isInteger(res)) {
+    return res;
+  } else {
+    return +res.toFixed(1);
+  }
 };
 
 // Do not edit below this line

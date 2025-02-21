f5c57567b251ec333bfa0dad3e959c5cccdf097a
yamchips <wflunch@gmail.com>
2025-02-21 10:08:40 +0000

    Finish js exercise 03

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let len = string.length;
+    let res = '';
+    for (let i = len - 1; i >= 0; i--) {
+        res += string.at(i);
+    }
+    return res;
 };
 
 // Do not edit below this line

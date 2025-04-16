1bae64bd8442bdd747b84fec58f1198378444fa2
yamchips <wflunch@gmail.com>
2025-04-16 17:24:01 +0100

    Start validation exercise

---
+++ javascript/Form-Validation/exercise-js.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const country = document.querySelector('#country');
+const postcode = document.querySelector('#postcode');
+const password = document.querySelector('#password');
+const confirmation = document.querySelector('#confirmation');
+

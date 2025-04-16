2ba3553979c1f999b9a7a7542d3578ee2f64e9d8
yamchips <wflunch@gmail.com>
2025-04-16 15:36:36 +0100

    Finish form validation tutorial

---
+++ javascript/Form-Validation/javascript.js
@@ -0,0 +1,38 @@
+const form = document.querySelector("form");
+const email = document.getElementById("mail");
+const emailError = document.querySelector("#mail + span.error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = ""; // Remove the message content
+    emailError.className = "error"; // Removes the `active` class
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  // if the email field is invalid
+  if (!email.validity.valid) {
+    // display an appropriate error message
+    showError();
+    // prevent form submission
+    event.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    // If empty
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    // If it's not an email address,
+    emailError.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    // If the value is too short,
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+  // Add the `active` class
+  emailError.className = "error active";
+}
